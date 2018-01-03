c82cf8fe7683bb3522a5e48d13ff6236babf6dff
Ludics <ludi7319@163.com>
2018-01-04 00:29:38 +0800

    modify add_note

--- add_note.php
+++ add_note.php
@@ -1,5 +1,16 @@
 <?php
-  $result = file_get_contents('php://input');
-  echo $result;
-  echo json_decode(json_encode($result));
+include 'header.php';
+$result = file_get_contents('php://input');
+$obj = json_decode(json_encode($result));
+$userID = $obj->{'userID'};
+$note = $obj->{"note"};
+$text = $obj->{"text"};
+$bookname = $obj->{"bookname"};
+
+$obj_re->noteID = 100;
+$obj_re->userID = $userID;
+$data = json_encode($obj_re);
+echo $data; 
+myLOG($userID . $note . $text . $bookname);
+
 ?>
